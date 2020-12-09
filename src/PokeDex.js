6dc12e6098c292e1e37a0d4d9bbbd411ae4c6d5e
Lucia Cerchie <luciacerchie@gmail.com>
2020-12-09 10:16:59 -0700

    step 4 complete

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -1,39 +1,28 @@
-import React, { useState } from "react";
-import { v4 as uuid } from "uuid";
-import axios from "axios";
+import React from "react";
+import { useAxios } from "./hooks/useAxios";
 import PokemonSelect from "./PokemonSelect";
 import PokemonCard from "./PokemonCard";
 import "./PokeDex.css";
 
-/* Renders a list of pokemon cards.
- * Can also add a new card at random,
+/* Renders a list of playing cards.
+ * Can also remove all cards,
+ * and add a new card, either at random
  * or from a dropdown of available pokemon. */
 function PokeDex() {
-  const [pokemon, setPokemon] = useState([]);
-  const addPokemon = async name => {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name}/`
-    );
-    setPokemon(pokemon => [...pokemon, { ...response.data, id: uuid() }]);
-  };
-  return (
+  const [pokemon, addPokemon, clearPokemon] = useAxios( //grabbing responses, add responses, clear res from useAxios
+    "pokemon", //this str will be formatted as keyinLS
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+  return ( //returning deck using funcs from usAxios
     <div className="PokeDex">
       <div className="PokeDex-buttons">
         <h3>Please select your pokemon:</h3>
         <PokemonSelect add={addPokemon} />
+        <button onClick={clearPokemon}>Delete the pokemon!</button>
       </div>
       <div className="PokeDex-card-area">
-        {pokemon.map(cardData => (
-          <PokemonCard
-            key={cardData.id}
-            front={cardData.sprites.front_default}
-            back={cardData.sprites.back_default}
-            name={cardData.name}
-            stats={cardData.stats.map(stat => ({
-              value: stat.base_stat,
-              name: stat.stat.name
-            }))}
-          />
+        {pokemon.map(card => ( //mapping the responses to the cards
+          <PokemonCard key={card.id} {...card} />
         ))}
       </div>
     </div>
--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,15 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import uuid from "uuid";
-const useAxios = (url) => {
-    const [cards, setCards] = useState([]);
-    const addCard = async () => {
-        const response = await axios.get(
-          url
-        );
-        return  setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-} 
-return [cards, addCard];
-}
 
-export default useAxios;
+function useAxios(keyInLS, baseUrl) {
+    const [responses, setResponses] = useLocalStorage(keyInLS);
+  
+    const addResponseData = async (formatter = data => data, restOfUrl = "") => {
+      const response = await axios.get(`${baseUrl}${restOfUrl}`);
+      setResponses(data => [...data, formatter(response.data)]);
+    };
+  
+    const clearResponses = () => setResponses([]);
+  
+    return [responses, addResponseData, clearResponses];
+  }
+  
+  function useLocalStorage(key, initialValue = []) {
+    if (localStorage.getItem(key)) {
+      initialValue = JSON.parse(localStorage.getItem(key));
+    }
+    const [value, setValue] = useState(initialValue);
+  
+    useEffect(() => {
+      localStorage.setItem(key, JSON.stringify(value));
+    }, [value, key]);
+  
+    return [value, setValue];
+  }
+  
+  export default useLocalStorage;
+  
+  export {useAxios, useLocalStorage };
+
