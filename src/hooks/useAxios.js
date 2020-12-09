8e7b9b0a4ea16c51a4e2ad37df7c787d145d6632
Lucia Cerchie <luciacerchie@gmail.com>
2020-12-09 08:03:17 -0700

    step 3 complete

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import axios from "axios";
+import uuid from "uuid";
+const useAxios = (url) => {
+    const [cards, setCards] = useState([]);
+    const addCard = async () => {
+        const response = await axios.get(
+          url
+        );
+        return  setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+} 
+return [cards, addCard];
+}
+
+export default useAxios;
